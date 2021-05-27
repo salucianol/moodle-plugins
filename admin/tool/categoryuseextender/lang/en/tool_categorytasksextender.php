d8baa7a9fa72e953bcd2a087d62895cb988c37c6
Samuel Alejandro Luciano Lassis <sa.lassis@gmail.com>
2021-05-26 21:42:26 -0400

    Create the basic plugin structure. Add lib.php & version.php.

---
+++ admin/tool/categoryuseextender/lang/en/tool_categorytasksextender.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+ 
+/**
+ * @package   tool_categorytasksextender
+ * @copyright 2021, Samuel Luciano <sa.lassis@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$string['pluginname'] = 'Category Tasks Extender';
+$string['backuplinktext'] = 'Backup courses from';
+$string['restorelinktext'] = 'Restore courses into';
+$string['categorytasksextender:backupcategorycourses'] = 'Create courses backups from category';
+$string['categorytasksextender:restorecoursescategory'] = 'Restore courses backups into category';
---
+++ admin/tool/categoryuseextender/lib.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+ 
+/**
+ * @package   tool_categorytasksextender
+ * @copyright 2021, Samuel Luciano <sa.lassis@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+function tool_categorytasksextender_extend_navigation_category_settings(navigation_node $parentnode, context_coursecat $context){
+    $categoryid = $context->instanceid;
+    if(has_capability('tool/categorytasksextender:backupcategorycourses', $context)){
+        $linktext = get_string('backuplinktext', 
+                                'tool_categorytasksextender');
+        $link = new moodle_url($CFG->wwwroot.'/admin/tool/categorytasksextender/backup.php',
+                                array('categoryid'=>$categoryid));
+        $node = navigation_node::create($linktext, 
+                                            $link, 
+                                            navigation_node::TYPE_SETTING, 
+                                            null, 
+                                            null, 
+                                            new pix_icon('i/settings', ''));
+        $parentnode->add_node($node);
+    }
+    if(has_capability('tool/categorytasksextender:restorecoursescategory', $context)){
+        $linktext = get_string('restorelinktext', 
+                                'tool_categorytasksextender');
+        $link = new moodle_url($CFG->wwwroot.'/admin/tool/categorytasksextender/backup.php',
+                                array('categoryid'=>$categoryid));
+        $node = navigation_node::create($linktext, 
+                                            $link, 
+                                            navigation_node::TYPE_SETTING, 
+                                            null, 
+                                            null, 
+                                            new pix_icon('i/settings', ''));
+        $parentnode->add_node($node);
+    }
+}
